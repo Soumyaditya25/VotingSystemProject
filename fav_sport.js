b53e4c0eea44a57ae02ef28cc71cf3b77836dafe
SaiBharadwaja570 <saibharadwaja1906@gmail.com>
2024-12-18 16:45:49 +0530

    fav_sport.js updated

--- fav_sport.js
+++ fav_sport.js
@@ -1,35 +1,8 @@
-let form = document.querySelector("form");
-const pollData = JSON.parse(localStorage.getItem("pollData")) || { votes: {}, voters: {} };
 
 
-function addVote(userEmail, favSport) {
-    if (pollData.voters[userEmail]) {
-        alert("You have already voted!");
-        return false;
-    }
-
-
-    pollData.votes[favSport] = (pollData.votes[favSport] || 0) + 1;
-
-
-    pollData.voters[userEmail] = favSport;
-
-    localStorage.setItem("pollData", JSON.stringify(pollData));
-    alert("Vote submitted successfully!");
-    return true;
-}
-
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const data = new FormData(form);
-    const userEmail = localStorage.getItem("loggedInUser"); 
-    let favSport;
-
-    for (const entry of data) {
-        favSport = entry[1]; 
-    }
-
-    if (userEmail && favSport) {
-        addVote(userEmail, favSport);
-    } 
-});
+document.getElementById('submit').addEventListener('click', (e) => {
+    e.preventDefault();
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+    console.log(users);
+    
+})
