2dcb7fc667d7c765681e5944698b96f153f194bc
chaitanya bunny <chaitanyagunjapalli@gmail.com>
2024-12-18 21:48:37 +0530

    added

---
+++ fav_sport.js
@@ -0,0 +1,27 @@
+document.getElementById('poll1Form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+
+    const userEmail = localStorage.getItem('userEmail');
+    const selectedSport = document.querySelector('input[name="sport"]:checked')?.value;
+
+
+    if (!selectedSport) {
+        alert('Please select an option!');
+        return;
+    }
+
+    const userData = JSON.parse(localStorage.getItem('userPollData')) || {};
+ 
+
+    if (!userData[userEmail]) {
+        userData[userEmail] = { pollResponses: {} };
+    }
+
+    userData[userEmail].pollResponses['poll1'] = selectedSport;
+
+    localStorage.setItem('userPollData', JSON.stringify(userData));
+
+    alert('Your vote has been submitted!');
+    window.location.href = 'mainPage.html'; 
+});
---
+++ sign_up.js
@@ -0,0 +1,34 @@
+let signup = document.querySelector("#signup");
+
+signup.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const password = document.getElementById("password").value;
+
+
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+
+    const userAvailable = users.some(
+        (user) => user.email === email || user.phone === phone
+    );
+
+    if (userAvailable) {
+        alert("User Already Exists!");
+    } else {
+        let userData = {
+            name: name,
+            email: email, 
+            phone: phone,
+            password: password, 
+        };
+
+        users.push(userData);
+        localStorage.setItem("users", JSON.stringify(users));
+
+        alert("User Registered Successfully!");
+        window.location.href = "mainPage.html"; 
+    }
+});
